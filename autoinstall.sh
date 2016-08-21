33f289def983b05f554aa13b5ffb0df4e3576d9f
Cano <gcanoxl@gmail.com>
2016-08-21 19:19:01 +0800

    update

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,7 +1,8 @@
+rm -rf ~/.config/nvim
+mkdir ~/.config/nvim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir ~/.config/nvim
 
 wget https://github.com/gcanoxl/dotfiles/blob/master/neovim/init.vim ~/.config/nvim/init.vim
 nvim +PlugInstall
